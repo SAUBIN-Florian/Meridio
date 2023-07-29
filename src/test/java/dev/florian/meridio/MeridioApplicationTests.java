fe5e220d3439bc3b892cccbab1676cb832b9b28c
SAUBIN-Florian <saubinflorian@gmail.com>
2023-07-29 08:18:53 +0200

    Initializing Meridio project

---
+++ src/test/java/dev/florian/meridio/MeridioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.florian.meridio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeridioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
